7598249257e5ec8769ca155c249533cdf78998b3
nashgao <nashgao1992@gmail.com>
2021-07-01 22:27:40 +1000

    remove listener annotation

--- src/Listener/ServerIdListener.php
+++ src/Listener/ServerIdListener.php
@@ -4,13 +4,9 @@ declare(strict_types=1);
 
 namespace Nashgao\MQTT\Listener;
 
-use Hyperf\Event\Annotation\Listener;
 use Hyperf\Event\Contract\ListenerInterface;
 use Hyperf\Framework\Event\BeforeMainServerStart;
 
-/**
- * @Listener
- */
 class ServerIdListener implements ListenerInterface
 {
     public static string $serverId;
